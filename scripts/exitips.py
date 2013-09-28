2e380fb7e4eb03bcd6b007232a31af7d6939241c
Arlo Breault <arlolra@gmail.com>
2013-09-27 18:57:41 -0700

    read multiple consensuses

--- scripts/exitips.py
+++ scripts/exitips.py
@@ -3,57 +3,94 @@
 import json
 import operator
 
+from os import listdir
+from dateutil.parser import parse
+from dateutil.tz import tzlocal
+from datetime import datetime
+from math import floor
+
 from stem.descriptor import parse_file
 from stem.exit_policy import AddressType
 
 
 class Router():
-    def __init__(self, router):
+    def __init__(self, router, tminus):
+        self.Fingerprint = router.fingerprint
         self.Address = router.address
         self.IsAllowedDefault = router.exit_policy._is_allowed_default
         self.Rules = []
+        self.Tminus = tminus
 
-exit_addresses = {}
-for descriptor in parse_file("data/exit-addresses", "tordnsel 1.0"):
-    descriptor.exit_addresses.sort(key=operator.itemgetter(1), reverse=True)
-    exit_addresses[descriptor.fingerprint] = descriptor.exit_addresses[0][0]
 
-server_descriptors = {}
-for descriptor in parse_file("data/all_descriptors", "server-descriptor 1.0"):
-    server_descriptors[descriptor.fingerprint] = descriptor.exit_policy
+exits = {}
+now = datetime.now(tzlocal())
+consensuses = listdir("data/consensuses")
+consensuses.sort(reverse=True)
+exit_lists = listdir("data/exit-lists")
 
-with open("data/exit-policies", "w") as exit_file:
-    for router in parse_file("data/consensus",
+for f in consensuses:
+
+    # strip -consensus
+    d = f[:-10]
+
+    # consensus from t hours ago
+    t = int(floor((now - parse(d)).total_seconds() / 3600))
+
+    # read in consensus and store routes in exits
+    for router in parse_file("data/consensuses/" + f,
                              "network-status-consensus-3 1.0"):
+        if router.fingerprint in exits:
+            continue
         if router.exit_policy.is_exiting_allowed():
-            r = Router(router)
-            if router.fingerprint in exit_addresses:
-                r.Address = exit_addresses[router.fingerprint]
-            if router.fingerprint in server_descriptors:
-                for x in server_descriptors[router.fingerprint]._get_rules():
-                    is_address_wildcard = x.is_address_wildcard()
-                    mask = None
-                    if not is_address_wildcard:
-                        address_type = x.get_address_type()
-                        if (address_type == AddressType.IPv4 and
-                            x._masked_bits != 32) or \
-                            (address_type == AddressType.IPv6 and
-                                x._masked_bits != 128):
-                            mask = x.get_mask()
-                    r.Rules.append({
-                        "IsAddressWildcard": is_address_wildcard,
-                        "Address": x.address,
-                        "Mask": mask,
-                        "IsAccept": x.is_accept,
-                        "MinPort": x.min_port,
-                        "MaxPort": x.max_port
-                    })
-            else:
-                for x in router.exit_policy._get_rules():
-                    r.Rules.append({
-                        "IsAddressWildcard": True,
-                        "IsAccept": x.is_accept,
-                        "MinPort": x.min_port,
-                        "MaxPort": x.max_port
-                    })
-            exit_file.write(json.dumps(r.__dict__) + "\n")
+            r = Router(router, t)
+            for x in router.exit_policy._get_rules():
+                r.Rules.append({
+                    "IsAddressWildcard": True,
+                    "IsAccept": x.is_accept,
+                    "MinPort": x.min_port,
+                    "MaxPort": x.max_port
+                })
+            exits[router.fingerprint] = r
+
+    # get a corresponding exit list
+    m = [x for x in exit_lists if x.startswith(d[:-5])]
+    if len(m) == 0:
+        continue
+
+    # update exit addresses with data from TorDNSEL
+    for descriptor in parse_file("data/exit-lists/" + m[0], "tordnsel 1.0"):
+        descriptor.exit_addresses.sort(key=operator.itemgetter(1),
+                                       reverse=True)
+        e = exits.get(descriptor.fingerprint, None)
+        if e is not None and e.Tminus == d:
+            e.Address = descriptor.exit_addresses[0][0]
+
+# update all with server descriptor info
+for descriptor in parse_file("data/cached-descriptors",
+                             "server-descriptor 1.0"):
+    if descriptor.fingerprint in exits:
+        rules = []
+        for x in descriptor.exit_policy._get_rules():
+            is_address_wildcard = x.is_address_wildcard()
+            mask = None
+            if not is_address_wildcard:
+                address_type = x.get_address_type()
+                if (address_type == AddressType.IPv4 and
+                    x._masked_bits != 32) or \
+                    (address_type == AddressType.IPv6 and
+                        x._masked_bits != 128):
+                    mask = x.get_mask()
+            rules.append({
+                "IsAddressWildcard": is_address_wildcard,
+                "Address": x.address,
+                "Mask": mask,
+                "IsAccept": x.is_accept,
+                "MinPort": x.min_port,
+                "MaxPort": x.max_port
+            })
+        exits[descriptor.fingerprint].Rules = rules
+
+# output exits to file
+with open("data/exit-policies", "w") as exit_file:
+    for e in exits:
+        exit_file.write(json.dumps(exits[e].__dict__) + "\n")
