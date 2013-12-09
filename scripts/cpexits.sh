f4f102e3e3d6f065910f2388b3bf87bd753de2e9
Arlo Breault <arlolra@gmail.com>
2013-12-08 22:39:27 -0800

    Purge old data
    48hrs should be sufficient.

--- scripts/cpexits.sh
+++ scripts/cpexits.sh
@@ -5,8 +5,12 @@ TORDATA=/srv/tor
 DNSEL=/srv/tordnsel.torproject.org/state
 NOW=$(date +"%Y-%m-%d-%H-%M-%S")
 
+find $CHECK/data/exit-lists -type f -mtime +1 -delete
 cat $DNSEL/exit-addresses $DNSEL/exit-addresses.new > $CHECK/data/exit-lists/$NOW
+
+find $CHECK/data/consensuses -type f -mtime +1 -delete
 cp $TORDATA/cached-consensus $CHECK/data/consensuses/$NOW-consensus
+
 cat $TORDATA/cached-descriptors $TORDATA/cached-descriptors.new > $CHECK/data/cached-descriptors
 
 cd $CHECK
